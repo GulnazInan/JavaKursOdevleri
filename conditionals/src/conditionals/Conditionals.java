c43e86234609e95b95732ea6628aa79490b22a31
GulnazInan <gulnaz.inan@hotmail.com>
2022-09-22 02:39:52 +0300

    Dosyalar Tekrar Yüklendi
    İsim değişikliği gerçekleştirildi

---
+++ conditionals/src/conditionals/Conditionals.java
@@ -0,0 +1,29 @@
+package conditionals;
+
+public class Conditionals {
+
+	public static void main(String[] args) {
+		
+		int sayi = 20;
+		if (sayi<20) {
+			System.out.println("Sayı 20 den küçüktür");
+		}
+		
+		else {
+			System.out.println("Sayı 20den büyüktür");
+				
+		}
+		if (sayi== 20) {
+			 System.out.println("Sayı 20ye eşittir");
+		
+		 }
+		
+		
+			
+			
+		}
+    
+			
+	}
+
+
